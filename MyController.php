c1fea7cc9d8581a8c80a1fb419df470c5e34c161
kyle <kyle@mathersit.com.au>
2021-10-26 13:40:51 +1100

    more info on controller.

--- MyController.php
+++ MyController.php
@@ -6,7 +6,7 @@ use Espo\Core\Api\Request;
 
 class MyController extends \Espo\Core\Controllers\Record
 {
-    public function postActionAbc(Request $request): bool
+    public function postActionAbc(Request $request): bool //return type is optional and can be bool, array, StdClass, other?
     {
 		$data = $request->getParsedBody();
 		$params = $request->getRouteParams();
