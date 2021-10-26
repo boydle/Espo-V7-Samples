da024f6ef461150d400bd9c2ee4696073117cb3d
kyle <kyle@mathersit.com.au>
2021-10-26 14:12:02 +1100

    service factory updated for v7

--- ServiceFactory.php
+++ ServiceFactory.php
@@ -3,24 +3,19 @@
 namespace Espo\Modules\ListPlus\Controllers;
 
 use Espo\Core\{
-Container,
+Di,
 Api\Request
 };
 
-class ListPlusAdmin
+class ListPlusAdmin implements Di\ServiceFactoryAware
 {
-    protected $container;
-
-    public function __construct(Container $container)
-    {
-        $this->container = $container;
-    }
+    use Di\ServiceFactorySetter;
 
     public function postActionCreateScopeBackEndChanges(Request $request) {
         // extract the payload from the request object
         $dataObj = $request->getParsedBody();
         // invoke service class to execute the instructions passing the payload as object
-        $result = $this->container->get('serviceFactory')->create('ListPlusAdmin')->createScopeBackEndChanges($dataObj);
+        $result = $this->serviceFactory->create('ListPlusAdmin')->createScopeBackEndChanges($dataObj);
         return $result;
     }
 }
