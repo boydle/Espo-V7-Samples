6c4d9827f450bfb81503b5fabb3af52e5768d2e7
kyle <kyle@mathersit.com.au>
2021-10-26 14:14:19 +1100

    Direct injection updates for v7

--- getEntityManager.php
+++ getEntityManager.php
@@ -3,32 +3,29 @@
 namespace Espo\Modules\Esignature\Controllers;
 
 use Espo\Core\{
-Container,
-Exceptions\BadRequest,
+Di,
 Api\Request
 };
 
-class Esignature
+class Esignature implements Di\ServiceFactoryAware, Di\EntityManagerAware
 {
-    protected $container;
 
-    public function __construct(Container $container)
-    {
-        $this->container = $container;
-    }
+    use Di\ServiceFactorySetter;
+
+    use Di\EntityManagerSetter;
 
     public function postActionPrintForEsignature(Request $request): string
     {
         $data = $request->getParsedBody();
-        $entity = $this->container->get('entityManager')->getEntity($data->entityType,$data->entityId);
-        $template = $this->container->get('entityManager')->getEntity('Template',$data->templateId);
+        $entity = $this->entityManager->getEntity($data->entityType,$data->entityId);
+        $template = $this->entityManager->getEntity('Template',$data->templateId);
         if(empty($entity)) {
             throw new BadRequest('Entity "{$data->entityType}" ID "{$data->entityId}" not found');
         }
         if(empty($template)) {
             throw new BadRequest('Template ID "{$data->templateId}" not found');
         }
-        $result = $this->container->get('serviceFactory')->create('Esignature')->printForEsignature($entity, $template, $data->isPortal);
+        $result = $this->serviceFactory->create('Esignature')->printForEsignature($entity, $template, $data->isPortal);
         return $result;
     }
 
