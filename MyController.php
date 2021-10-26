6af231c130a392ba83d38faac2e8d628caf03ef6
kyle <kyle@mathersit.com.au>
2021-10-26 13:36:36 +1100

    Controller Action V6

---
+++ MyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Espo\Modules\MyExt\Controllers;
+
+use Espo\Core\Api\Request;
+
+class MyController extends \Espo\Core\Controllers\Record
+{
+    public function postActionAbc($params, $data, $request)
+    {
+		//do stuff
+    }
+
+}
