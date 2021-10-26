11d2dbb7bb780c80b6a57bcb5e60438ab3fb3a3e
kyle <kyle@mathersit.com.au>
2021-10-26 13:16:38 +1100

    entry point v7

--- EntryPoint.php
+++ EntryPoint.php
@@ -3,13 +3,14 @@
 namespace Espo\Modules\MyExt\EntryPoints;
 
 use Espo\Core\Api\Request;
+use Espo\Core\Api\Response;
 
 class EpName extends \Espo\Core\EntryPoints\Base
 {
     public static $authRequired = true;
 
     // default action
-    public function run(Request $request)
+    public function run(Request $request, Response $response): void
     {
 		//STUFF
     }
